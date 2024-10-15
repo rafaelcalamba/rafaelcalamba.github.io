292802e225a477980b8c476aa602d2c6e1b0d64f
rafaelcalamba <calambarafael@gmail.com>
2024-10-15 11:52:42 +0800

    Add Gene-o-cide embed, add SCP logo, remove collapsible, center everything

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('gameButton').onclick = function () { showGameFrame() };
-document.getElementById('musicButton').onclick = function () { showMusicFrame() };
-function showGameFrame() {
-    document.getElementById('gameFrame').style.display = 'block';
-}
-function showMusicFrame() {
-    document.getElementById('musicFrame').style.display = 'block';
-}
